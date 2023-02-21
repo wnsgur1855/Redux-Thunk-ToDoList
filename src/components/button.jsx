b58290aa26e249fb2f723e0a9f20505fcde342db
junhyuck park <wnsgur1855@naver.com>
2023-02-21 22:46:21 +0900

    컴포넌트 분리

---
+++ src/components/button.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const StBtn = styled.button`
+  width: 100%;
+  height: 50px;
+  background-color: wheat;
+  border: none;
+  border-radius: 20px;
+
+  text-align: center;
+`;
+
+export const backButtonHandler = () => {
+  alert('빽');
+};

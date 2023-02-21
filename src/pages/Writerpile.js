cd67697cd363dff8ef1ce3433094135ced1d627d
junhyuck park <wnsgur1855@naver.com>
2023-02-21 22:45:52 +0900

    Writer 리덕스 툴킷 청크로 수정

--- src/pages/Writerpile.js
+++ src/pages/Writerpile.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import useInput from '../hooks/useInput';
 import styled from 'styled-components';
-import { StBtn } from './Home';
+import { StBtn } from '../components/button';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { createAdd } from '../redux/modules/todoSlice';
-import axios from 'axios';
 import api from '../axios/api';
+import { __postTodo } from '../redux/modules/todoSlice';
 
 const Stp = styled.p`
   font-size: x-large;
@@ -17,20 +17,10 @@ const StInputBox = styled.input`
 
 function Writerpile() {
   //useInput-------------------------------------------------------------------------------------
-  //커스텀 훅
-  //inputValue가 객체이니까 형태 맞게 변형
-  const [writer, writerHandler] = useInput({
-    title: '',
-  });
-  const [title, titleHandler] = useInput({
-    title: '', //-->객체 형식만 맞추면 되는거라 content쓸 필요 ㄴ
-  });
-  const [content, contentHandler] = useInput({
-    title: '',
-  });
-  //   console.log(writer);
-  //   console.log(title);
-  //   console.log(content);
+  const [writer, writerHandler] = useInput('');
+  const [title, titleHandler] = useInput('');
+  const [content, contentHandler] = useInput('');
+
   //useNavigate-------------------------------------------------------------------------------------
   const navigate = useNavigate();
   //useDispatch-------------------------------------------------------------------------------------
@@ -42,10 +32,12 @@ function Writerpile() {
   //     dispatch(createAdd({ writer, title, content }));
   //   };
   //POST추가기능-------------------------------------------------------------------------------------
-  const onSubmitHandler = async () => {
+  //db에 데이터가 어떤 형식으로 쌓이는지 잘 파악하고 post
+  const onSubmitHandler = async (newTodo) => {
     //이 줄이 안 끝나고 저 navigate가 실행이 되니까 새로고침을 해야지 보여지는 것이다 await로 실행까지 기다리게 하자
-    await api.post('/todos', writer, title, content);
+    //객체안에는 key(e.target.value)를 가져왔어서 안 됐다. :객체 자체를 추가해줘야한다.
     navigate('/list');
+    dispatch(__postTodo(newTodo));
   };
 
   //-------------------------------------------------------------------------------------
@@ -55,7 +47,12 @@ function Writerpile() {
         onSubmit={(e) => {
           e.preventDefault();
           //버튼 클릭시, input에 들어가는 값(state)을 이용하여 db에 저장(post요청)
-          onSubmitHandler();
+          const newTodo = {
+            // id: Date.now(), 상세페이지 만들때 쓴다
+            title,
+            content,
+          };
+          onSubmitHandler(newTodo);
         }}
       >
         <Stp>작성자</Stp>

0018de3772dfd4ba06c0c6fae5a29520e642098e
junhyuck park <wnsgur1855@naver.com>
2023-02-21 22:44:59 +0900

    useInput수정

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -5,10 +5,7 @@ const useInput = () => {
 
   const handler = (e) => {
     //db의 todos는 객체 형태이므로 이렇게 써줘야한다
-    setValue({
-      title: e.target.value,
-      content: e.target.value,
-    });
+    setValue(e.target.value);
   };
   return [value, handler]; //--->return위치 똑바로 하기
 };
